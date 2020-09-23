904a13a6602bf43f6fae18921a7d3ee05b218977
Marvin Buss <marvin.buss@gmail.com>
2020-09-23 18:04:46 +0200

    added update JSON script, updated workflow

---
+++ code/UpdateJson.ps1
@@ -0,0 +1,20 @@
+# Define script arguments
+param (
+    [Parameter(Mandatory = $true)]
+    [String[]]
+    $FilePaths,
+
+    [Parameter(Mandatory = $true)]
+    [String]
+    $ParameterName,
+
+    [Parameter(Mandatory = $true)]
+    [String]
+    $ParameterValue
+)
+
+foreach ($FilePath in $FilePaths) {
+    $JsonContent = Get-Content -Path $FilePath -Raw | Out-String | ConvertFrom-Json
+    $JsonContent.parameters.$ParameterName.value = $ParameterValue
+    $JsonContent | ConvertTo-Json -Depth 32| Set-Content $FilePath
+}
